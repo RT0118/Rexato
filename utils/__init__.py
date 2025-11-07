8a9696c5f4b988bc5795a55eab97c8753de23e21
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-11-07 14:06:00 +0800

    Update init file to use new function names

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,14 +2,14 @@
 Utility functions for data processing
 """
 
-from .rekordbox_utils import get_rekordbox_tracks, load_rekordbox_playlists
-from .serato_utils import load_serato_crates
+from .rekordbox_utils import load_rekordbox_playlists
+from .serato_utils import load_serato_crates, get_available_serato_subcrates_dir
 from .metadata_utils import get_audio_metadata, get_basic_metadata, format_time
 
 __all__ = [
-    'get_rekordbox_tracks',
     'load_rekordbox_playlists',
     'load_serato_crates',
+    'get_available_serato_subcrates_dir',
     'get_audio_metadata',
     'get_basic_metadata',
     'format_time'
